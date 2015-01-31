adf543e92c57c2ce482462c27c89c643f2341cd4
joaodiogocosta <jdscosta91@gmail.com>
2015-01-31 19:28:31 +0000

    Change query string

--- lib/season/active_record/scopes.rb
+++ lib/season/active_record/scopes.rb
@@ -1,29 +1,29 @@
 module Scopes
   def self.included(base)
     base.class_eval do
-      def self.created_before( date )
-        where( :conditions => [ "#{table_name}.created_at < ?", date ] )
+      def self.created_before(date)
+        where("#{table_name}.created_at < ?", date)
       end
 
-      def self.created_between( start_date, end_date )
-        where( :conditions => [ "#{table_name}.created_at > ? AND #{table_name}.created_at < ?", start_date, end_date ] )
+      def self.created_between(start_date, end_date)
+        where("#{table_name}.created_at > ? AND #{table_name}.created_at < ?", start_date, end_date)
       end
 
-      def self.created_after( date )
-        where( :conditions => [ "#{table_name}.created_at > ?", date ] )
+      def self.created_after(date)
+        where("#{table_name}.created_at > ?", date)
       end
 
 
-      def self.updated_before( date )
-        where( :conditions => [ "#{table_name}.updated_at < ?", date ] )
+      def self.updated_before(date)
+        where("#{table_name}.updated_at < ?", date)
       end
 
-      def self.updated_between( start_date, end_date )
-        where( :conditions => [ "#{table_name}.updated_at > ? AND #{table_name}.updated_at < ?", start_date, end_date ] )
+      def self.updated_between(start_date, end_date)
+        where("#{table_name}.updated_at > ? AND #{table_name}.updated_at < ?", start_date, end_date)
       end
 
-      def self.updated_after( date )
-        where( :conditions => [ "#{table_name}.updated_at > ?", date ] )
+      def self.updated_after(date)
+        where("#{table_name}.updated_at > ?", date)
       end
     end
   end
