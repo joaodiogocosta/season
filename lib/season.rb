efa60838e9c4c8e69c91aa3f3d1568561dfceb90
joaodiogocosta <jdscosta91@gmail.com>
2015-01-31 19:08:34 +0000

    Fix load code placement

--- lib/season.rb
+++ lib/season.rb
@@ -2,11 +2,11 @@ require "season/version"
 require "season/active_record/scopes"
 
 module Season
-  
-  if defined? ActiveRecord
-    class ActiveRecord::Base
-      include Scopes
-    end
-  end
 
+end
+
+if defined? ActiveRecord
+  class ActiveRecord::Base
+    include Scopes
+  end
 end
