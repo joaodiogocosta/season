591354abda58b3d6da69cead9bfd4b23551e1fba
joaodiogocosta <jdscosta91@gmail.com>
2015-02-09 22:48:09 +0000

    Add season scopes for all datetime columns and preserve legacy usage

--- lib/season.rb
+++ lib/season.rb
@@ -1,3 +1,32 @@
 require "season/version"
 require "season/configuration"
-require "season/active_record/scopes" if defined? ActiveRecord
+require "season/legacy"
+require "season/scope_builder"
+require "season/query_builder"
+
+module Season
+
+  QUERY_VERBS = ['before', 'after', 'between']
+
+  def self.included(base)
+    base.extend(ClassMethods)
+
+    mb = ScopeBuilder.new(base)
+
+    base.class_eval do
+      # DEFINE SCOPES DINAMICALLY
+      base.datetime_column_names.each do |column_name|
+        QUERY_VERBS.each do |query_verb|
+          mb.build(table_name, column_name, query_verb)
+        end
+      end
+    end
+  end
+
+  module ClassMethods
+    def datetime_column_names
+      columns.map { |c| c.name if c.type == :datetime }.compact
+    end
+  end
+end
+
--- lib/season/configuration.rb
+++ lib/season/configuration.rb
@@ -23,7 +23,8 @@ module Season
 
     def self.auto_include_scopes
       if configuration.include_by_default
-        ActiveRecord::Base.send(:include, Season::Scopes) if defined? ActiveRecord
+        warn "[DEPRECATED] Include season scopes by default is deprecated and will be removed before v0.5 - See https://github.com/joaodiogocosta/season for more"
+        ActiveRecord::Base.send(:include, Season::Legacy) if defined? ActiveRecord
       end
     end
 end
